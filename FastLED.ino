18ed51ffa38841bd909b23bf9c34222602a6a64a
Simon Christmann <simon@christmann.email>
2018-04-25 17:33:51 +0200

    Initial Animation support

--- FastLED.ino
+++ FastLED.ino
@@ -1,19 +1,29 @@
+#define FASTLED_INTERVAL  (1000 / FASTLED_FPS)
+
 NeoPixelBus<NeoGrbFeature, NeoEsp8266Dma800KbpsMethod> strip(FASTLED_NUM_LEDS);
 CRGB leds[FASTLED_NUM_LEDS];
 
 Thread ledOutputThread = Thread();
+Thread ledAnimationThread = Thread();
 
 uint8_t hue = 0;
 uint8_t sat = 0;
 uint8_t bri = 0;
+uint8_t ani = 2;
+String aniStr = "confetti";
 
 void setup_FastLED() {
   strip.Begin();
   strip.Show();  
 
   ledOutputThread.onRun(ledOutput);
-  ledOutputThread.setInterval(1000 / FASTLED_FPS);
+  ledOutputThread.setInterval(FASTLED_INTERVAL);
   threadControl.add(&ledOutputThread);
+
+  ledAnimationThread.onRun(ledAnimationController);
+  ledAnimationThread.setInterval(FASTLED_INTERVAL);
+  ledAnimationThread.enabled = false;
+  threadControl.add(&ledAnimationThread);
 }
 void setup_FastLED_Network() {
   mqtt.subscribe(s+MQTT_PREFIX+"/set/"+BOARD_ID+"/fastled", light_subscribe);
@@ -25,7 +35,12 @@ void light_subscribe(String topic, String message) {
   JsonVariant root = jsonBuffer.parse(message);
 
   if ( root.is<float>() || root.is<int>() ) {
+    ledAnimationThread.enabled = false;
     setBri( root.as<float>() );
+    
+    ledSolidColor(hue, sat, bri);
+    publishLight();
+    return;
   }
   if ( root.is<JsonObject>() ) {
     JsonObject& rootObject = root.as<JsonObject>();
@@ -38,10 +53,21 @@ void light_subscribe(String topic, String message) {
     if ( rootObject.containsKey("sat") ) {
       setSat( rootObject["sat"].as<float>() );
     }
+    if ( rootObject.containsKey("animation") ) {
+      aniStr = rootObject["animation"].as<String>();
+      if      (aniStr == "sinelon")    { ani = 1; }
+      else if (aniStr == "confetti")   { ani = 2; }
+      else if (aniStr == "colorwheel") { ani = 3; }
+      
+      ledAnimationThread.enabled = true;
+      publishLight();
+      return;
+    } else {
+      ledAnimationThread.enabled = false;
+    }
+    ledSolidColor(hue, sat, bri);
+    publishLight();
   }
-
-  ledSolidColor();
-  publishLight();
 }
 void publishLight() {
   String output;
@@ -49,8 +75,12 @@ void publishLight() {
 
   JsonObject& root = jsonBuffer.createObject();
   root["val"] = rescale(bri, 255, 1.0);
-  root["hue"] = rescale(hue, 255, 1.0);
-  root["sat"] = rescale(sat, 255, 1.0);
+  if (ledAnimationThread.enabled) {
+    root["animation"] = aniStr;
+  } else {
+    root["hue"] = rescale(hue, 255, 1.0);
+    root["sat"] = rescale(sat, 255, 1.0);
+  }
 
   root.printTo(output);
   mqtt.publish(s+MQTT_PREFIX+"/status/"+BOARD_ID+"/fastled", output, true);
@@ -75,6 +105,21 @@ float setBri(float val) {
   return bri;
 }
 
+void ledAnimationController() {
+  hue++;
+  switch(ani) {
+    case 1:
+      ledAnimationLoop_Sinelon(13);
+      break;
+    case 2:
+      ledAnimationLoop_Confetti();
+      break;
+    case 3:
+      ledSolidColor(hue, sat, bri);
+      break;
+  }
+}
+
 void ledOutput() { 
   RgbColor pixel;
   for (int i = 0; i < FASTLED_NUM_LEDS; i++) {
@@ -84,6 +129,17 @@ void ledOutput() {
   strip.Show();  
 }
 
-void ledSolidColor() {
-  fill_solid(leds, FASTLED_NUM_LEDS, CHSV( hue, sat, bri));
+void ledSolidColor(uint8_t hue, uint8_t sat, uint8_t bri) {
+  fill_solid(leds, FASTLED_NUM_LEDS, CHSV(hue, sat, bri));
 }
+void ledAnimationLoop_Sinelon(uint8_t bpm) {
+  fadeToBlackBy(leds, FASTLED_NUM_LEDS, FASTLED_INTERVAL);
+  uint16_t pos = beatsin16(bpm, 0, FASTLED_NUM_LEDS-1);
+  leds[pos] += CHSV(hue, 255, bri);
+}
+void ledAnimationLoop_Confetti() {
+  fadeToBlackBy(leds, FASTLED_NUM_LEDS, FASTLED_INTERVAL/2);
+  uint16_t pos = random16(FASTLED_NUM_LEDS);
+  leds[pos] += CHSV(hue+random8(64), 255, bri);
+}
+
